12b384053efc0da7238efb585e2c786ac3be892f
pbroestl <pbroestl@gmail.com>
2017-04-30 20:35:09 -0700

    Basic enemy Ai for movement, and more stuff

--- app/src/main/java/com/csc780/eppb/tbd/NeetGame.java
+++ app/src/main/java/com/csc780/eppb/tbd/NeetGame.java
@@ -14,9 +14,17 @@ public class NeetGame extends Game {
 
     public static final int V_WIDTH = 1024;
     public static final int V_HEIGHT = 560;
-
     //Pixels per Meter, used for scaling in relation to B2Body
     public static final float PPM  = 100.0f;
+
+    //Collision bits for filtering game Objects;
+    public static final short DEFAULT_BIT = 1;
+    public static final short CHARACTER_BIT = 2;
+    public static final short ENEMY_BIT = 4;
+    public static final short ATTACK_BIT = 8;
+    public static final short ENEMYATTACK_BIT = 16;
+    public static final short TARGET_BIT = 32;
+    public static final short RANGE_BIT = 64;
 
     private BattleScreen screen;
     public SpriteBatch batch ;
